7d747576c231a84e5c4ff6338a3d3b24c1709d0a
salesandmarketing <test@test.test>
2022-05-05 12:27:11 +0500

    Change sqlite to postgres

--- main.py
+++ main.py
@@ -9,15 +9,18 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegistrationForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# get from od with config Vars in heroku, via Settings - Reveal Config Vars, change 'sqlite:///blog.db' and '8BYkEfBA6O6donzWlSihBXox7C0sKR6b' as SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
